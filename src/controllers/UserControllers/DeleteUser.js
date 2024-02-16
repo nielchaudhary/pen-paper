c1aa33a21f5cc55862982f82a331cfa891842a1e
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-02-17 03:28:54 +0530

    made changes to route

--- src/controllers/UserControllers/DeleteUser.js
+++ src/controllers/UserControllers/DeleteUser.js
@@ -1,15 +1,16 @@
+const bcrypt = require('bcrypt');
 const User = require('../../models/UserModel');
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
 const deleteUser = async (req, res) => {
     try {
-
-        //validation
+        // Validation
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const { username, password } = req.body;
+
+        const { username } = req.body;
 
         // Check if a user with the provided username exists
         const existingUser = await User.findOne({ username });
@@ -17,12 +18,7 @@ const deleteUser = async (req, res) => {
             return res.status(400).json({ error: 'User does not exist' });
         }
 
-        // Check if the provided password matches the stored password (you should use a secure hashing mechanism)
-        if (existingUser.password !== password) {
-            return res.status(401).json({ error: 'Invalid password' });
-        }
-
-        // If username and password are valid, delete the user
+        // Delete the user
         await User.findOneAndDelete({ username });
 
         res.json({ message: 'User deleted successfully' });
