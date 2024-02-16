2efe0a695b6ac7daffc24c51f2543c7c2a243174
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-02-17 03:26:07 +0530

    added hashing for passwords

--- src/controllers/UserControllers/CreateNewUser.js
+++ src/controllers/UserControllers/CreateNewUser.js
@@ -1,14 +1,15 @@
+const bcrypt = require('bcrypt');
 const User = require('../../models/UserModel');
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
 const createUser = async (req, res) => {
     try {
-
-        //validation
+        // Validation
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
         const { username, password } = req.body;
 
         // Check if a user with the same username already exists
@@ -17,19 +18,17 @@ const createUser = async (req, res) => {
             return res.status(400).json({ error: 'Username already exists' });
         }
 
-        // If the username is unique, create a new user
-        const newUser = new User({ username, password });
-        res.status(200).json({
-            message : 'User Successfully Created',
-            username : newUser.username,
-            password : newUser.password,
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
 
-        });
-
-
+        // Create a new user with hashed password
+        const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
 
-
+        res.status(200).json({
+            message: 'User Successfully Created',
+            username: newUser.username,
+        });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
--- src/controllers/UserControllers/LoginUser.js
+++ src/controllers/UserControllers/LoginUser.js
@@ -1,28 +1,25 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../../models/UserModel');
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 require("dotenv").config();
 const secretKey = process.env.SECRET_KEY;
 
 const loginUser = async (req, res) => {
     try {
-        //validation
+        // Validation
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
         const { username, password } = req.body;
+
         // Find the user in the database by their username
-        const user = await User.findOne({ username: username });
+        const user = await User.findOne({ username });
 
-        // If the user is not found, send an error response
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-
-        // If the password does not match, send an error response
-        if (user.password !== password) {
+        // If the user is not found, or password doesn't match, send an error response
+        if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
@@ -30,7 +27,7 @@ const loginUser = async (req, res) => {
         const token = jwt.sign({ userId: user._id, username: user.username }, secretKey, { expiresIn: "1h" });
 
         // Set the JWT token as a cookie
-        res.cookie('jwtToken', token, { maxAge: 3600000, httpOnly: true }); // Expires in 1 hour (3600000 milliseconds)
+        res.cookie('jwtToken', token, { maxAge: 3600000, httpOnly: true });
 
         // Respond with success message and JWT token
         res.json({ Success: `${user.username} Logged In Successfully.`, JWT: token });
