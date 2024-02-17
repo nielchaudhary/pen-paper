a32ea2b9de575fd60d4db44095d0a22b3da55811
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-02-18 04:02:37 +0530

    overall directory commit

--- src/middleware/RateLimiter.js
+++ src/middleware/RateLimiter.js
@@ -5,7 +5,7 @@ const app = express();
 // Set up basic rate limiting (adjust as needed)
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: 15,
     message: 'Too many requests from this IP, please try again later.',
 });
 
