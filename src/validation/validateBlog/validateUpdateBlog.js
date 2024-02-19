44d06f65d3ce232a8f4c2ec28fd16cbe689b0ae8
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-02-19 17:24:28 +0530

    added cookie validation

--- src/validation/validateBlog/validateUpdateBlog.js
+++ src/validation/validateBlog/validateUpdateBlog.js
@@ -1,7 +1,10 @@
-const { body ,query} = require('express-validator');
+const { body ,query,cookie} = require('express-validator');
 
 const validateUpdateBlog = [
     // Validate title, content, and category fields
+    cookie('jwtToken').notEmpty().withMessage('JWT token is required'),
+
+
     query('blogId').notEmpty().withMessage('BlogId is required'),
     body('title').notEmpty().withMessage('Title is required'),
     body('content').notEmpty().withMessage('Content is required'),
